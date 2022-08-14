3076be520120a1bee2cd4f90839a63cb7931eafd
Evans Ochieng <evansochieng121@gmail.com>
2022-08-14 11:51:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+
+// returnFirstTwoDrivers() Function
+const returnFirstTwoDrivers = function(drivers) {
+    return [drivers[0], drivers[1]];
+};
+
+// returnLastTwoDrivers() Function
+const returnLastTwoDrivers = (drivers) => {
+    return [drivers[drivers.length - 2], drivers[drivers.length - 1]];
+};
+
+// selectingDrivers array
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// createFareMultiplier() Function
+function createFareMultiplier(number) {
+    // return function(fare) {
+    //     return fare * number;
+    // }; OR
+    return fare => fare * number;
+};
+
+// fareDoubler() Function
+//pass 2 as the value to number parameter coz we're doubling fare
+const fareMultiplier = createFareMultiplier(2); 
+function fareDoubler(fare) {
+    return fareMultiplier(fare);
+};
+
+// fareTripler() Function
+// pass 3 as the argument to createFareMultiplier function
+const multiplyFare = createFareMultiplier(3);
+function fareTripler(fare) {
+    return multiplyFare(fare);
+};
+
+// selectDifferentDrivers() Function
+const selectDifferentDrivers = (drivers, returnDrivers) => {
+    return returnDrivers(drivers);
+};
